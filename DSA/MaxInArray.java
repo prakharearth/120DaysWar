648664eda131d07dbb4aa742716f496bd256de99
Prakhar Raj <126399247+prakharearth@users.noreply.github.com>
2026-02-18 10:05:25 +0530

    Add MaxInArray problem solution

---
+++ DSA/MaxInArray.java
@@ -0,0 +1,12 @@
+public class MaxInArray {
+  public static void main(String[] args){
+    int[] arr= {5,2,9,1,7};
+    int max = arr[0];
+    for (int i = 1; i< arr.length; i++){
+        if(arr[i]> max){
+          max = arr[i];
+        }
+    }
+    System.out.println("Maximum element is: " + max);
+  }
+}
